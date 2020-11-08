cdd0f9b5d2e159ac82c1a2802f61679797a8a122
shibilm22 <shibil.mandawe@patelhs.org>
2020-11-08 18:43:41 +0000

    deleted a space

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 class Main {
-
   public static void main(String[] args) {
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     int[] names = new int[15];
