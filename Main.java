d09a955c879538c22e9445b729e6b284abd390b9
shibilm22 <shibil.mandawe@patelhs.org>
2020-11-08 19:12:29 +0000

    added string array

--- Main.java
+++ Main.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
 class Main {
-public static void main(String[] args) {
+  public static void main(String[] args) {
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    //These are the amount of seats
-    int[] names = new int[15];
-    //Amount of random student names
+    // These are the amount of seats
     Scanner scan = new Scanner(System.in);
-    //Generate 15 random names
-    for (int i = 0; i < names.length; i++){
-    //seats start at index of 0 (1)
-    //the seats have to be less than the length of names
-    //You can only +1 to the index if it is less than the length of names
-    System.out.println("Enter a name"); 
-    //Every time you enter a name, the statement pops up 15 more times
-    names[i] = scan.next();
+     // Amount of random student names
+    String [] names = new String [15];
+    // Generate 15 random names
+    for (int i = 0; i < names.length; i++) {
+      // seats start at index of 0 (1)
+      // the seats have to be less than the length of names
+      // You can only +1 to the index if it is less than the length of names
+      System.out.println("Enter a name");
+      names[i] = scan.next();
+      // Every time you enter a name, the statement pops up 15 more times
     }
     int i = 0;
-    while (i < names.length);{
-    System.out.println(names [i] + "Sits in seat number" + seats [i]);
+    while (i < names.length)
+    {
+      System.out.println(names[i] + "Sits in seat number" + seats[i]);
     }
-    
-}
-  
+
   }
-    
-  
+
+}
