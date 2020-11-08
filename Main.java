d260c0fc6ac63ec7e0ef6282a134193c6304f79a
shibilm22 <shibil.mandawe@patelhs.org>
2020-11-08 18:45:58 +0000

    added comment

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ class Main {
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     //These are the amount of seats
     int[] names = new int[15];
+    //Amount of random student names
     Scanner scan = new Scanner(System.in);
+    //Generate 15 random names
     for (int i = 0; i < names.length; i++)
       System.out.println("whats is your name?");
    String name = scan.next();
